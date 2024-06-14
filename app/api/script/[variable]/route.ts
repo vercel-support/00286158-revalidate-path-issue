9a0d7c1e57f0f506610d44f12e3b57b45b150aa5
Thibaut <thibautdumont@pm.me>
2024-06-14 18:43:12 +0200

    test with revalidate export instead

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -1,14 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const revalidate = 3600; // revalidate the data at most every hour
+
 export async function GET(
   request: NextRequest,
   params: { params: { variable: string } }
 ): Promise<NextResponse> {
   console.log("Regenerating script with variable", params.params.variable);
 
-  return new NextResponse(`hello variable ${params.params.variable}`, {
-    headers: {
-      "Vercel-CDN-Cache-Control": "max-age=86400", // 1 day
-    },
-  });
+  return new NextResponse(`hello variable ${params.params.variable}`);
 }
