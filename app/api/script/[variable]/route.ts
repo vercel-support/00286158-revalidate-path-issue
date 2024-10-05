41bc36df61b23837344722bbfe85273c51c84f31
Nick Vigier <nvigier46@gmail.com>
2024-10-05 14:28:00 +0100

    Refactor cache control headers in [variable]/route.ts

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -1,20 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+export const revalidate = 600;
+
 export async function GET(
   request: NextRequest,
   params: { params: { variable: string } }
 ): Promise<NextResponse> {
   console.log('Regenerating script with variable', params.params.variable);
 
+  const posts = await fetch('https://jsonplaceholder.org/posts').then((res) =>
+    res.json()
+  );
+
+  console.log('Fetched posts', posts);
+
   return new NextResponse(
     `hello variable ${params.params.variable} ; date: ${Date.now()}`,
-    {
-      status: 200,
-      headers: {
-        'Cache-Control': 'max-age=600, must-revalidate', // 1 day
-        'Vercel-CDN-Cache-Control': 'max-age=60',
-        'CDN-Cache-Control': 'max-age=10',
-      },
-    }
+    // {
+    //   status: 200,
+    //   headers: {
+    //     'Cache-Control': 'max-age=600, must-revalidate', // 1 day
+    //     'Vercel-CDN-Cache-Control': 'max-age=60',
+    //     'CDN-Cache-Control': 'max-age=10',
+    //   },
+    // }
   );
 }
