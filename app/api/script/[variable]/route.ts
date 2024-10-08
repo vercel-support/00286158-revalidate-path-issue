0f4d94298608e0361b8dec228f8007100ac2c6b2
Nick Vigier <nvigier46@gmail.com>
2024-10-08 10:25:27 +0100

    test revalidate: false in fetch opts

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -9,14 +9,16 @@ export async function GET(
 ): Promise<NextResponse> {
   console.log('Regenerating script with variable', params.params.variable);
 
-  const posts = await fetch('https://jsonplaceholder.org/posts').then((res) =>
-    res.json()
-  );
+  const posts = await fetch('https://jsonplaceholder.org/posts', {
+    next: {
+      revalidate: false,
+    },
+  }).then((res) => res.json());
 
   console.log('Post1:', posts[0]);
 
   return new NextResponse(
-    `hello variable ${params.params.variable} ; date: ${Date.now()}`,
+    `hello variable ${params.params.variable} ; date: ${Date.now()}`
     // {
     //   headers: {
     //     'Vercel-CDN-Cache-Control': 'max-age=86400', // 1 day
