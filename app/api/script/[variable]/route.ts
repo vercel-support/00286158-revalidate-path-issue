95ef9402d4528e9e94d37740fe827efe182a9ef7
Nick Vigier <nvigier46@gmail.com>
2024-10-08 10:10:08 +0100

    Testing pages directory and res.revalidate

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -1,38 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const revalidate = 600;
-
-export const generateStaticParams = async () => [
-  {
-    variable: 'test',
-  },
-];
-
 export async function GET(
   request: NextRequest,
   params: { params: { variable: string } }
 ): Promise<NextResponse> {
   console.log('Regenerating script with variable', params.params.variable);
 
-  const posts = await fetch('https://jsonplaceholder.org/posts', {
-    next: {
-      revalidate: 600,
-    }
-  }).then((res) =>
+  const posts = await fetch('https://jsonplaceholder.org/posts').then((res) =>
     res.json()
   );
 
-  console.log('Fetched posts', posts);
+  console.log('Post1:', posts[0]);
 
   return new NextResponse(
-    `hello variable ${params.params.variable} ; date: ${Date.now()}`
-    // {
-    //   status: 200,
-    //   headers: {
-    //     'Cache-Control': 'max-age=600, must-revalidate', // 1 day
-    //     'Vercel-CDN-Cache-Control': 'max-age=60',
-    //     'CDN-Cache-Control': 'max-age=10',
-    //   },
-    // }
+    `hello variable ${params.params.variable} ; date: ${Date.now()}`,
+    {
+      headers: {
+        'Vercel-CDN-Cache-Control': 'max-age=86400', // 1 day
+      },
+    }
   );
 }
---
+++ pages/api/purge-script-cache/[variable]/index.ts
@@ -0,0 +1,34 @@
+// import { revalidatePath } from 'next/cache';
+// import { NextRequest, NextResponse } from 'next/server';
+
+// export async function GET(
+//   request: NextRequest,
+//   params: { params: { variable: string } }
+// ): Promise<NextResponse> {
+//   console.log('Purge variable script cache');
+
+//   revalidatePath(`/api/script/${params.params.variable}`);
+//   revalidatePath(`/api/script/[variable]`, 'page');
+
+//   // return new NextResponse(`purged /api/script/${params.params.variable}`);
+//   return new NextResponse(
+//     `purged /api/script/[variable] and /api/script/${params.params.variable}`
+//   );
+// }
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    console.log(req.query.variable);
+    
+    await res.revalidate(`/api/script/${req.query.variable}`);
+
+    res.status(200).json({ revalidated: true });
+  } catch (err) {
+    res.status(500).json({ error: 'failed to revalidate' });
+  }
+}
