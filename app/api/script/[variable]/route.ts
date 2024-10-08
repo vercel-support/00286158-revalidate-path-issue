9573c4183f5e5fda7b01466dfdf501f27e3b2f66
Nick Vigier <nvigier46@gmail.com>
2024-10-08 11:29:57 +0100

    Test without GSP

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { revalidateTag } from 'next/cache';
 
 async function fetchScriptContent(variable: string) {
   const res = await fetch(
@@ -21,24 +20,14 @@ export async function GET(
   return new NextResponse(scriptContent, {
     headers: {
       'Content-Type': 'text/plain',
-      // 'Vercel-CDN-Cache-Control': 'max-age=86400', // 1 day
     },
   });
 }
-
-// export async function POST(
-//   request: NextRequest,
-//   { params }: { params: { variable: string } }
-// ) {
-//   const tag = `script-${params.variable}`;
-//   revalidateTag(tag);
-//   return NextResponse.json({ revalidated: true, now: Date.now() });
-// }
 
 export const dynamic = 'force-static';
 export const revalidate = false;
 export const dynamicParams = false;
 
-export function generateStaticParams() {
-  return [{ variable: '1' }, { variable: '2' }, { variable: '3' }];
-}
+// export function generateStaticParams() {
+//   return [{ variable: '1' }, { variable: '2' }, { variable: '3' }];
+// }
