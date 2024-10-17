f4f1977ef0a902375563d68353f0e90c10e94efc
Nick Vigier <nvigier46@gmail.com>
2024-10-17 08:31:48 +0100

    remove app directory

--- app/node/route.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET(req: NextRequest) {
-  return NextResponse.json({ runtime: 'node', test: 'test' });
-}
